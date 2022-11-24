322f912ad2d05c68b31e3179a039e92ee21efb59
menuHwang <hmin0206@naver.com>
2022-11-24 20:40:52 +0900

    완주하지 못한 선수 : 해결

---
+++ src/완주하지_못한_선수/Solution.java
@@ -0,0 +1,20 @@
+package 완주하지_못한_선수;
+
+import java.util.HashMap;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for (String name : completion) {
+            if (map.get(name) == 1) map.remove(name);
+            else map.put(name, map.get(name) - 1);
+        }
+
+        return map.keySet().iterator().next();
+    }
+}
