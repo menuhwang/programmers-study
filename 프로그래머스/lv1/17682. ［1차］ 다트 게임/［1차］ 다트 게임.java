c89f94fa6f9106729d1d042509a03557486b20d8
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-03-07 11:38:01 +0900

    [level 1] Title: [1차] 다트 게임, Time: 2.75 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,12 +1,8 @@
 import java.util.regex.*;
 
 class Solution {
-    final String SCORE_REGEX = "\\d{1,2}";
-    final String BONUS_REGEX = "[S,D,T]{1}";
-    final String OPTION_REGEX = "[*,#]?";
-    final Pattern SCORE_PATTERN = Pattern.compile(SCORE_REGEX);
-    final Pattern BONUS_PATTERN = Pattern.compile(BONUS_REGEX);
-    final Pattern OPTION_PATTERN = Pattern.compile(OPTION_REGEX);
+    final String REGEX = "(\\d{1,2})([S|D|T]{1})([*|#]?)";
+    final Pattern PATTERN = Pattern.compile(REGEX);
     final String BONUS = " SDT";
     
     public int solution(String dartResult) {
@@ -14,30 +10,29 @@ class Solution {
         int[] score = new int[4];
         int answer = 0;
         
-        Pattern p = Pattern.compile(SCORE_REGEX + BONUS_REGEX + OPTION_REGEX);
-        Matcher m = p.matcher(dartResult);
+        Matcher m = PATTERN.matcher(dartResult);
         
-        for (int i = 1; i < 4; i++) {
-            if (m.find()) {
-                String round = m.group();
-                RoundResult roundResult = new RoundResult(round);
-                score[i] = roundResult.getScore();
-                switch (roundResult.getOption()) {
-                    case "*":
-                        score[i - 1] *= 2;
-                        score[i] *= 2;
-                        break;
-                    case "#":
-                        score[i] *= -1;
-                        break;
-                    default:
-                        break;
-                }
+        for (int i = 1; i < 4 && m.find(); i++) {
+            String round = m.group();
+            RoundResult roundResult = new RoundResult(round);
+            score[i] = roundResult.getScore();
+            switch (roundResult.getOption()) {
+                case "*":
+                    score[i - 1] *= 2;
+                    score[i] *= 2;
+                    break;
+                case "#":
+                    score[i] *= -1;
+                    break;
+                default:
+                    break;
             }
         }
+        
         for (int i = 1; i < 4; i++) {
             answer += score[i];
         }
+        
         return answer;
     }
     
@@ -47,14 +42,12 @@ class Solution {
         private String option;
         
         public RoundResult(String roundResultStr) {
-            Matcher roundMatcher = SCORE_PATTERN.matcher(roundResultStr);
-            if (roundMatcher.find()) this.score = Integer.parseInt(roundMatcher.group());
-            
-            roundMatcher.usePattern(BONUS_PATTERN);
-            if (roundMatcher.find()) this.bonus = BONUS.indexOf(roundMatcher.group());
-            
-            roundMatcher.usePattern(OPTION_PATTERN);
-            if (roundMatcher.find()) this.option = roundMatcher.group();
+            Matcher roundMatcher = PATTERN.matcher(roundResultStr);
+            if (roundMatcher.find()) {
+                this.score = Integer.parseInt(roundMatcher.group(1));
+                this.bonus = BONUS.indexOf(roundMatcher.group(2));
+                this.option = roundMatcher.group(3);
+            }
         }
         
         public int getScore() {
