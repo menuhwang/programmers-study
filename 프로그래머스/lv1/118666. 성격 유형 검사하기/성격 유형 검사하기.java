9e5da3618bff3b9453a5fe9c7c3cfb4682846554
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-03-16 10:51:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.09 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        int[] point = {0, -3, -2, -1, 0, 1, 2, 3};
+        int RT = 0;
+        int CF = 0;
+        int JM = 0;
+        int AN = 0;
+        // RT -+
+        // CF -+
+        // JM -+
+        // AN -+
+        
+        for (int i = 0; i < survey.length; i++) {
+            String type = survey[i];
+            int choice = point[choices[i]];
+            switch (survey[i]) {
+                case "RT":
+                case "TR":
+                    RT += type.equals("RT") ? choice : -1 * choice;
+                    break;
+                    
+                case "CF":
+                case "FC":
+                    CF += type.equals("CF") ? choice : -1 * choice;
+                    break;
+                    
+                case "JM":
+                case "MJ":
+                    JM += type.equals("JM") ? choice : -1 * choice;
+                    break;
+                    
+                case "AN":
+                case "NA":
+                    AN += type.equals("AN") ? choice : -1 * choice;
+                    break;
+                    
+                default:
+            }
+        }
+        
+        String answer = "";
+        answer += RT <= 0 ? "R" : "T";
+        answer += CF <= 0 ? "C" : "F";
+        answer += JM <= 0 ? "J" : "M";
+        answer += AN <= 0 ? "A" : "N";
+        return answer;
+    }
+}
