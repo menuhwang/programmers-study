4b5074876b8a0c7c00a96a87a6258210f339dcf2
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-04-09 11:46:02 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 18980 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] array;
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        array = new int[M];
+
+        dfs(0, 1);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static void dfs(int depth, int s) throws IOException {
+        if (depth == M) {
+            for (int num : array) {
+                bw.write(num + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = s; i <= N; i++) {
+            array[depth] = i;
+            dfs(depth + 1, i);
+        }
+    }
+}
