be273859185588b673aad951f92534d671ef74a5
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-04-11 13:50:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 892 ms, Memory: 91932 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int R;
+    static Queue<Integer> queue = new LinkedList<>();
+    static PriorityQueue<Integer>[] list;
+    static int[] index;
+    static int num = 1;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        
+        list = new PriorityQueue[N + 1];
+        index = new int[N + 1];
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            if (list[x] == null) list[x] = new PriorityQueue<>(Comparator.reverseOrder());
+            if (list[y] == null) list[y] = new PriorityQueue<>(Comparator.reverseOrder());
+            
+            list[x].offer(y);
+            list[y].offer(x);
+        }
+        
+        bfs(R);
+        
+        while(!queue.isEmpty()) bfs(queue.poll());
+        
+        for (int i = 1; i < index.length; i++) bw.write(index[i] + "\n");
+        
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    
+    private static void bfs(int node) {
+        if (index[node] > 0) return;
+        index[node] = num++;
+        
+        while(list[node] != null && !list[node].isEmpty()) queue.offer(list[node].poll());
+    }
+}
