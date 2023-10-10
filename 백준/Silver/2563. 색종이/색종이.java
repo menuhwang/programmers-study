54c66079914a31686a964e8340b197af95009484
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-10-10 11:11:14 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,35 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[][] map = new boolean[100][100];
-        int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-        int x;
-        int y;
-        for (int n = 0; n < N; n++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            for (int i = y; i < y + 10; i++) {
-                for (int j = x; j < x + 10; j++) {
-                    map[i][j] = true;
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+            /*
+            * 100 * 100의 도화지
+            * false: 색종이 x
+            * true: 색종이 o
+            */
+            boolean[][] board = new boolean[101][101];
+
+            // P: paper
+            int P = Integer.parseInt(br.readLine());
+
+            // 색종이 부착
+            StringTokenizer st;
+            for (int p = 0; p < P; p++) {
+                st = new StringTokenizer(br.readLine());
+                int col = Integer.parseInt(st.nextToken());
+                int row = Integer.parseInt(st.nextToken());
+
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        board[row + i][col + j] = true;
+                    }
                 }
             }
+
+            // 넓이 계산
+            int count = 0;
+            for (boolean[] row : board)
+                for (boolean col : row)
+                    if (col)
+                        count++;
+
+            bw.write(count +"");
+
+            bw.flush();
         }
 
-        int result = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (map[i][j]) result++;
-            }
-        }
-
-        System.out.println(result);
     }
 }
