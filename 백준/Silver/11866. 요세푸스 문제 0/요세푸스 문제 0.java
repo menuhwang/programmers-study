384abc5b01c120c15929e86b5417dabd1c3c42cf
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-10-24 13:50:25 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 176 ms, Memory: 24232 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,32 +1,38 @@
-import java.util.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        
-        Queue<Integer> queue = new LinkedList<>();
-        int[] answer = new int[N];
-        for (int i = 1; i <= N; i++) queue.offer(i);
-        
-        int index = 0;
-        int times = 1;
-        int temp;
-        while(!queue.isEmpty()) {
-            temp = queue.poll();
-            if (times++ % K != 0) queue.offer(temp);
-            else answer[index++] = temp;
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 1; i <= N; i++)
+                queue.offer(i);
+
+            bw.write("<");
+            int cnt = 1;
+            while (!queue.isEmpty()) {
+                if (cnt++ % K == 0) {
+                    bw.write(queue.poll() + "");
+                    if (!queue.isEmpty())
+                        bw.write(", ");
+                }
+                else {
+                    queue.offer(queue.poll());
+                }
+            }
+            bw.write(">");
+
+            bw.flush();
         }
-        
-        StringBuilder sb = new StringBuilder("<");
-        for (int i = 0; i < answer.length; i++) {
-            sb.append(answer[i]);
-            if (i < answer.length - 1) sb.append(", ");
-        }
-        sb.append(">");
-        
-        System.out.print(sb.toString());
     }
 }
