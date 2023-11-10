d3c960a3a8da8687a3c88d1733f4d0677d85314e
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2023-11-10 10:14:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,70 +1,70 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main {
-    static int count;
-    static boolean[][] matrix;
-    static int[] dx = new int[]{0, -1, 0, 1};
-    static int[] dy = new int[]{1, 0, -1, 0};
-    
+class Main {
+    private static boolean[][] map;
+    private static int tempApt;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        matrix = new boolean[N][N];
-        
-        PriorityQueue<Integer> apt = new PriorityQueue<>();
-        
-        String line;
-        
-        for (int i = 0; i < N; i++) {
-            line = br.readLine();
-            for (int c = 0; c < line.length(); c++) {
-                matrix[i][c] = line.charAt(c) == '1';
-            }
-        }
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (matrix[i][j]) {
-                    count = 0;
-                    dfs(i, j);
-                    apt.add(count);
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+            int N = Integer.parseInt(br.readLine());
+            map = new boolean[N][N];
+
+            for (int i = 0; i < N; i++) {
+                char[] row = br.readLine().toCharArray();
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = row[j] == '1';
                 }
             }
-        }
-        
-        int size = apt.size();
-        
-        bw.write(size + "\n");
-        for (int i = 0; i < size; i++) {
-            bw.write(apt.poll() + "\n");
-        }
-        
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-    
-    private static void dfs(int x, int y) {
-        matrix[x][y] = false;
-        count++;
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (isOnOfMatrix(nx, ny) && matrix[nx][ny]) {
-                dfs(nx, ny);
+
+            List<Integer> dan = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!map[i][j]) continue;
+                    tempApt = 0;
+                    dfs(i, j);
+                    dan.add(tempApt);
+                }
             }
+
+            Collections.sort(dan);
+            
+            bw.write(dan.size() + "\n");
+            for (int apt : dan)
+                bw.write(apt + "\n");
+
+            bw.flush();
         }
     }
-    
-    private static boolean isOnOfMatrix(int x, int y) {
-        return x < matrix.length
-            && y < matrix.length
-            && x > -1
-            && y > -1;
+
+    private static final int DIRECTION = 4;
+    private static final int[] dc = {0, 0, -1, 1};
+    private static final int[] dr = {-1, 1, 0, 0};
+
+    private static void dfs(int r, int c) {
+        map[r][c] = false;
+        tempApt++;
+
+        for (int i = 0; i < DIRECTION; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (!isOnMap(nr, nc)) continue;
+            if (!map[nr][nc]) continue;
+
+            dfs(nr, nc);
+        }
+    }
+
+    private static boolean isOnMap(int r, int c) {
+        return r > -1
+                && c > -1
+                && r < map.length
+                && c < map[0].length;
     }
 }
