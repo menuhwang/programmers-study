18e61a21c6ca3b847a68b9257a0081060f58bc6a
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2024-01-25 11:24:36 +0900

    [Silver III] Title: 주유소, Time: 412 ms, Memory: 36600 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,11 +9,10 @@ class Main {
         try (br; bw) {
             int N = Integer.parseInt(br.readLine());
 
-            long[] distanceFromStart = new long[N];
+            int[] distances = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i < N; i++) {
-                long distance = Long.parseLong(st.nextToken());
-                distanceFromStart[i] = distanceFromStart[i - 1] + distance;
+                distances[i] = Integer.parseInt(st.nextToken());
             }
 
             int[] prices = new int[N];
@@ -22,17 +21,21 @@ class Main {
                 prices[i] = Integer.parseInt(st.nextToken());
             }
 
-            long[] cost = new long[N];
-            Arrays.fill(cost, Long.MAX_VALUE);
-            cost[0] = 0;
-            for (int i = 0; i < N - 1; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    long distance = distanceFromStart[j] - distanceFromStart[i]; // 도착 - 출발
-                    cost[j] = Math.min(cost[j], prices[i] * distance + cost[i]);
-                }
+            int[] minPriceTo = new int[N];
+            int min = prices[0];
+            minPriceTo[1] = min;
+            for (int i = 2; i < N; i++) {
+                min = Math.min(min, prices[i - 1]);
+                minPriceTo[i] = min;
+            }
+            
+            long answer = 0;
+            for (int i = 1; i < N; i++) {
+                answer += (long) minPriceTo[i] * distances[i];
             }
 
-            bw.write(cost[N - 1] + "\n");
+            bw.write(answer + "\n");
+
 
             bw.flush();
         }
